9a4ae50be73cad516d4aac6aa2afd88ea485d354
Younyi Kim <rlakuku1221@gmail.com>
2024-04-22 21:52:45 +0900

    fix: Default Vite image 삭제

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -1,19 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import reactLogo from '@assets/react.svg';
-import viteLogo from '@public/vite.svg';
 import '@/App.css';
 
 function Root() {
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
 			<h1>Vite + React</h1>
 			<Outlet />
 		</>
