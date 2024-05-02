065dc812402c16cd627ef504375fc3524b87cee9
Younyi Kim <rlakuku1221@gmail.com>
2024-05-02 16:33:33 +0900

    feat: React Query Provider & DevTool 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
 import { RouterProvider } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 // Utils
 import router from './utils/router';
+import { queryClient } from '@/apis/react-query';
 
 function App() {
-	return <RouterProvider router={router} />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<div
+				style={{
+					fontSize: '16px',
+				}}
+			>
+				<ReactQueryDevtools initialIsOpen={false} />
+			</div>
+		</QueryClientProvider>
+	);
 }
 
 export default App;
